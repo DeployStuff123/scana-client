c7965e3f5fe1ff4489ed808b187647f2ae76582a
hamim2114 <hamim2114@gmail.com>
2025-08-09 14:47:15 +0600

    completed

--- utils/axiosReq.js
+++ utils/axiosReq.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = window.location.hostname === 'localhost'
   ? "http://localhost:5000"
-  : "https://litz-server.vercel.app";
+  : import.meta.env.VITE_SERVER_API;
 
 const apiReq = axios.create({
   baseURL: baseURL,
